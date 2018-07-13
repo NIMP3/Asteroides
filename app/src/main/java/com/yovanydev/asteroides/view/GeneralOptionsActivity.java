87187d09d47188a338692c6b02ed068c87e68f43
EDWIN ORBES <yovany.dev@gmail.com>
2018-07-12 22:07:32 -0500

    Implementado el (Acerca de la aplicación) y mejoras en la interfaz del Menu de Juego

---
+++ app/src/main/java/com/yovanydev/asteroides/view/GeneralOptionsActivity.java
@@ -0,0 +1,15 @@
+package com.yovanydev.asteroides.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yovanydev.asteroides.R;
+
+public class GeneralOptionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_general_options);
+    }
+}
