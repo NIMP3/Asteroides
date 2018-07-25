6f48d43b09eedd6b3408ad80d3991a712fef1388
EDWIN ORBES <yovany.dev@gmail.com>
2018-07-24 21:48:19 -0500

    Eventos onclick sobre el menu principal

--- app/src/main/java/com/yovanydev/asteroides/MenuActivity.java
+++ app/src/main/java/com/yovanydev/asteroides/MenuActivity.java
@@ -1,13 +1,46 @@
 package com.yovanydev.asteroides;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MenuActivity extends AppCompatActivity {
+import com.yovanydev.asteroides.view.GeneralOptionsActivity;
+
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Button buttonAbout = findViewById(R.id.btn_about);
+        Button buttonExit = findViewById(R.id.btn_exit);
+
+        buttonAbout.setOnClickListener(this);
+        buttonExit.setOnClickListener(this);
+    }
+    /*----------------------------------------------------------------------------------------------
+    * Lanzamos el Activity con un fragmento determinado*/
+    private void launchOptions(String option) {
+        Intent intent = new Intent(this, GeneralOptionsActivity.class);
+        intent.putExtra("option",option);
+        startActivity(intent);
+    }
+
+    /*----------------------------------------------------------------------------------------------
+    * Evento OnClick sobre los botones del menu*/
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_about:
+                launchOptions("ABOUT");
+                break;
+            case R.id.btn_exit:
+                finish();
+                break;
+        }
     }
 }
--- app/src/main/java/com/yovanydev/asteroides/view/GeneralOptionsActivity.java
+++ app/src/main/java/com/yovanydev/asteroides/view/GeneralOptionsActivity.java
@@ -1,9 +1,12 @@
 package com.yovanydev.asteroides.view;
 
+import android.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.yovanydev.asteroides.R;
+import com.yovanydev.asteroides.view.fragment.AboutFragment;
 
 public class GeneralOptionsActivity extends AppCompatActivity {
 
@@ -11,5 +14,26 @@ public class GeneralOptionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_general_options);
+
+        Bundle extras = getIntent().getExtras();
+        String option = extras.getString("option");
+
+        if (option!=null) {
+            switch (option) {
+                default:
+                    launchFragment(new AboutFragment());
+                    break;
+            }
+
+        }
+    }
+
+    /*----------------------------------------------------------------------------------------------
+    * Lanzar un fragmento determinado*/
+    private void launchFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.frameLayoutContainer, fragment)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .commit();
     }
 }
